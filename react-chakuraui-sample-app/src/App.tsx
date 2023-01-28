df314b38736695b3feae57d9f16daa94ba10dfb7
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-01-28 14:46:24 +0900

    test chakura UI

--- react-chakuraui-sample-app/src/App.tsx
+++ react-chakuraui-sample-app/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Button, ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button color="primary" borderRadius="none">
         テスト
       </Button>

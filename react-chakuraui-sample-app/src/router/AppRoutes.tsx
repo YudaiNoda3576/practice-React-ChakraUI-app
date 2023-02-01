6d3eacf3855bae2ba472ad5ffeba44b7beb59891
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-02-01 18:59:24 +0900

    ルーティングの修正

--- react-chakuraui-sample-app/src/router/AppRoutes.tsx
+++ react-chakuraui-sample-app/src/router/AppRoutes.tsx
@@ -5,18 +5,21 @@ import { Home } from "../components/pages/Home";
 import { UserManagement } from "../components/pages/UserManagement";
 import { Settings } from "../components/pages/Settings";
 import { NotFound } from "../components/pages/NotFound";
+import { Layout } from "../components/templates/Layout";
 
 export const AppRoutes = memo(() => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="home" element={<Outlet />}>
-          <Route index element={<Home />} />
-          <Route path="user_management" element={<UserManagement />} />
-          <Route path="settings" element={<Settings />} />
+        <Route path="" element={<Layout />}>
+          <Route path="/" element={<Login />} />
+          <Route path="home" element={<Outlet />}>
+            <Route index element={<Home />} />
+            <Route path="user_management" element={<UserManagement />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
         </Route>
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
